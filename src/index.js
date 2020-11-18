34978a227eec90c2ed624162b3337950eb8a8ba9
Mokshith-dev <mokshith.ajalade@gmail.com>
2020-11-18 10:39:02 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -63,7 +63,12 @@ app.put('/api/student/:id',(req,res) => {
         res.status(400).send("Invalid input");
         return;
     }
-    studentArray.splice(studentIndex,1,{id: parseInt(id),...req.body});
+    const studentObj = studentArray[studentIndex];
+    for (const key in req.body) {
+        
+            studentObj[key] = req.body[key];
+    }
+    //studentArray.splice(studentIndex,1,{id: parseInt(id),...req.body});
     res.status(200).send("Updated");
 
 });
